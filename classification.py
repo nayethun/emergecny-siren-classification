e209ea72e91fc636f546d3c86f8fef7958047549
Nathan Lee <ntlee1@bu.edu>
2024-03-26 10:21:36 -0400

    Added audio files + imports

--- classification.py
+++ classification.py
@@ -1,2 +1,9 @@
+import os
+import librosa
 import numpy as np
-import seaborn as sns 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+
+directories = ['path/to/ambulance_sirens', 'path/to/firetruck_sirens']
